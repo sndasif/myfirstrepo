6a281d8de6b8c440d0f838138ab7fd123346fdcb
md asif <118246712+sndasif@users.noreply.github.com>
2022-12-15 22:21:03 +0530

    new updation
    some functionalities have been added into the repository

--- vinod.js
+++ vinod.js
@@ -1,4 +1,11 @@
 let obj=document.querySelector(".class");
+let item=document.querySelector("#clone");
+sum=()=>{
+let a=10;b=50;
+ let total=(a+b);
+ console.log(total);
+}
+submit.addEventListener('click',sum());
 if(number=true){
  console.log("hello how are you");
 
